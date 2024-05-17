20466f98af0ee6fff768933ee6fde34cdff74887
Mustafa Alkamali <m7asanain@gmail.com>
2024-05-17 09:30:12 +0300

    Arabic and Turkish languages added

--- src/main/java/com/example/demo/config/appConfig.java
+++ src/main/java/com/example/demo/config/appConfig.java
@@ -2,8 +2,10 @@ package com.example.demo.config;
 
 import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,19 +18,45 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class appConfig implements WebMvcConfigurer{
+public class appConfig implements WebMvcConfigurer {
 
-    //here I literally copied Tamer Hoca's code from the app8 one
+	// here I literally copied Tamer Hoca's code from the app8 one
+	// @Bean
+	// public LocaleResolver localeResolver() {
+	// return new SessionLocaleResolver(); // new AcceptHeaderLocaleResolver();
+	// }
+
+	// @Override
+	// public void addInterceptors(InterceptorRegistry registry) {
+	// LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+	// interceptor.setParamName("language");
+	// registry.addInterceptor(interceptor);
+	// }
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
 	@Bean
 	public LocaleResolver localeResolver() {
-		return new SessionLocaleResolver(); // new AcceptHeaderLocaleResolver();
+		CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
+		return localeResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-		interceptor.setParamName("language");
-		registry.addInterceptor(interceptor);
+		registry.addInterceptor(localeChangeInterceptor());
 	}
-
 }
